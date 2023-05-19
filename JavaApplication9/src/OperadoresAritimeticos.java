3037219eebe7b702828d82ed4e5684a401188993
_victordeoliveiraa <116526849+victordeoliveira2000@users.noreply.github.com>
2023-05-18 22:37:00 -0400

    Add files via upload
    Arquivos das meus projeto feitos quando assistir os vídeos do curso em vídeo do professor Gustavo Guanabara

---
+++ JavaApplication9/src/OperadoresAritimeticos.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author vitin
+ */
+public class OperadoresAritimeticos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int n1 =3;
+        int n2 =5;
+        float m = (n1+n2)/2;
+        System.out.println("A média entre " + n1 +" e " + n2 + " é " + m);
+    }
+    
+}
---
+++ LeitorDePensamento/src/classes/Fonte.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classes;
+
+/**
+ *
+ * @author vitin
+ */
+public class Fonte {
+
+    static int PLAIN;
+
+    public Fonte() {
+    }
+
+    Fonte(String arial_black, int i) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    Fonte(String arial_black, int PLAIN, int i) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
